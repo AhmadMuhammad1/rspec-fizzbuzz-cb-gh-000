0a184220d215082379f3b58c9b57f81be4ebadb7
Ahmad Muhammad <ahm.shalaby1@gmail.com>
2018-09-03 23:06:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+    "FizzBuzz"
   elsif int % 3 == 0
-    return "Fizz"
+    "Fizz"
   elsif int % 5 == 0
-    return "Buzz"
+    "Buzz"
   end
